9ef8d6cb6a3bbecbd4889df0752968f4a19abd66
iciness <7602422+icinessx@users.noreply.github.com>
2019-07-08 10:34:26 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-cat >/etc/apt/sources.list <<EOF
-deb http://mirrors.163.com/debian/ stretch main non-free contrib
-deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib
-deb http://mirrors.163.com/debian/ stretch-backports main non-free contrib
-deb-src http://mirrors.163.com/debian/ stretch main non-free contrib
-deb-src http://mirrors.163.com/debian/ stretch-updates main non-free contrib
-deb-src http://mirrors.163.com/debian/ stretch-backports main non-free contrib
-deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
-deb-src http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
-EOF
-apt update
-apt install -y  apt-transport-https ca-certificates curl gnupg2 software-properties-common
+echo 'deb https://apt.dockerproject.org/repo debian-stretch main' >> /etc/apt/sources.list
+apt-get update
+apt-get install apt-transport-https ca-certificates curl gnupg2 lsb-release software-properties-common bash socat jq
 curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | apt-key add -
 echo "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian stretch stable" >>/etc/apt/sources.list
-apt update
-apt install -y docker-engine
+apt-get update
+apt-get install docker-engine
 systemctl start docker
 systemctl enable docker
