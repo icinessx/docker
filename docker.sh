8c70a7a3b8ebe86b2dbd82f0466e7e27029d94d8
iciness <7602422+icinessx@users.noreply.github.com>
2019-07-08 10:04:25 +0800

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cat >/etc/apt/sources.list <<EOF
+deb http://mirrors.163.com/debian/ stretch main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
+deb-src http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
+EOF
+apt update
+apt install -y  apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | apt-key add -
+echo "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian stretch stable" >>/etc/apt/sources.list
+apt update
+apt install -y docker-ce
+systemctl start docker
+systemctl enable docker
