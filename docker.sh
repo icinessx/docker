fc35a2f49d98a77312cac4c55728ebca5d33ef44
iciness <7602422+icinessx@users.noreply.github.com>
2019-07-08 10:55:50 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo 'deb https://apt.dockerproject.org/repo debian-stretch main' >> /etc/apt/sources.list
-apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg2 lsb-release software-properties-common bash socat jq
+apt update
+apt install apt-transport-https ca-certificates curl gnupg2 lsb-release software-properties-common bash socat jq
 curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | apt-key add -
 echo "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian stretch stable" >>/etc/apt/sources.list
-apt-get update
-apt updatesudo
+apt update
 apt install docker-ce
 systemctl start docker
 systemctl enable docker
